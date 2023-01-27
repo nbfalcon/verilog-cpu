9a50a80609cb7fe3d5800aa03c0ca36cade0a3c4
Nikita Bloshchanevich <nikblos@outlook.com>
2023-01-27 19:05:32 +0100

    bugfix: proper ordering for hex

--- tools-ng/src/HSBU/Genas/BinWriter.hs
+++ tools-ng/src/HSBU/Genas/BinWriter.hs
@@ -8,7 +8,7 @@ import Data.Bits
 import Data.Function
 
 hexEncodeW :: Word8 -> String
-hexEncodeW w = [ hexC `C8.index` lo, hexC `C8.index` hi ]
+hexEncodeW w = [ hexC `C8.index` hi, hexC `C8.index` lo ]
     where lo = fromIntegral $ w .&. 0xF
           hi = fromIntegral $ (w .&. 0xF0) `shiftR` 4
           hexC = C8.pack "0123456789ABCDEF"
